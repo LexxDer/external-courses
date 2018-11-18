d29e86a4cfaa0e34cd646c2581dcc2f1eab9775e
LexxDer <suchkovas2008@gmail.com>
2018-11-18 20:18:22 +0300

    unmerge

--- src/ex2_js-basics-part2/task05.js
+++
@@ -1,8 +0,0 @@
-var f = function(arr) {
-    var x = arr[0];
-    var i;
-    for (i = 1; i < arr.length; i++) {
-        if (arr[i] > x) x = arr[i];
-    }
-    console.log(`Максимальное значение: ${x}`);
-}
