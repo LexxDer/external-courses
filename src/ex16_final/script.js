b14c6f01d58fdefb2a225da5ff02de603602ec03
LexxDer <suchkovas2008@gmail.com>
2018-12-18 12:26:16 +0300

    18.12

--- src/ex16_final/script.js
+++ src/ex16_final/script.js
@@ -1,114 +1,192 @@
-var id = 0;
-function Book(cover, title, author, rating, views, year, price) {
-    this.cover = cover;
-    this.title = title;
-    this.author = author;
-    this.rating = rating;
-    this.views = views;
-    this.year = year;
-    this.price = price;
-    this.id = id;
-    var that = this;
-    id += 1;
+var booksData = [
+    {
+        imgUrl: 'https://images-na.ssl-images-amazon.com/images/I/51TYJDHEJSL._AC_UL320_SR248,320_.jpg',
+        title: 'Jewels of Nizam',
+        author: 'Geeta Devi',
+        rating: '3',
+        views: '20',
+        year: '2011',
+        price: '19.5'
+    },
+    {
+        imgUrl: 'https://b.zmtcdn.com/data/pictures/6/4000236/65f5b0154f1824897086b882a7060f8a_featured_v2.jpg',
+        title: 'Cakes & Bakes',
+        author: 'Sanjeev Kapoor',
+        rating: '1',
+        views: '4303',
+        year: '2013',
+        price: '22.3'
+    },
+    {
+        imgUrl: 'https://images-na.ssl-images-amazon.com/images/I/51FqIlv4TaL._SX258_BO1,204,203,200_.jpg',
+        title: 'Jamies Kitchen',
+        author: 'Jamie Oliver',
+        rating: '5',
+        views: '3054',
+        year: '2018',
+        price: '70.1'
+    },
+    {
+        imgUrl: 'https://www.freebiefindingmom.com/wp-content/uploads/2015/07/Cheap-meals-for-large-families-pin.jpg',
+        title: 'Inexpensive Family Meals',
+        author: 'Simon Holst',
+        rating: '3',
+        views: '3000',
+        year: '2013',
+        price: '110'
+    },
+    {
+        imgUrl: 'https://images-na.ssl-images-amazon.com/images/I/51z%2BXZgMFtL._SX384_BO1,204,203,200_.jpg',
+        title: 'Paleo Slow Cooking',
+        author: 'Chrissy Gower',
+        rating: '2',
+        views: '302',
+        year: '2001',
+        price: '5.3'
+    },
+    {
+        imgUrl: 'https://www.betterreading.com.au/wp-content/uploads/2014/10/9781921382192-195x298.jpg',
+        title: 'Cook Like an Italian',
+        author: 'Tobie Puttock',
+        rating: '3',
+        views: '22',
+        year: '2000',
+        price: '0'
+    },
+    {
+        imgUrl: 'https://images-na.ssl-images-amazon.com/images/I/51BiwDk0f3L._SX367_BO1,204,203,200_.jpg',
+        title: 'Indian Cooking',
+        author: 'Geeta Devi',
+        rating: '3',
+        views: '15',
+        year: '2007',
+        price: '30.2'
+    },
+    {
+        imgUrl: 'https://images-na.ssl-images-amazon.com/images/I/910r6WKtPfL.jpg',
+        title: 'Jamie Does',
+        author: 'Jamie Oliver',
+        rating: '1',
+        views: '345',
+        year: '2011',
+        price: '0'
+    },
+    {
+        imgUrl: 'https://images-na.ssl-images-amazon.com/images/I/61AkWIb6ArL._SX258_BO1,204,203,200_.jpg',
+        title: 'Jamies italy',
+        author: 'Jamie Oliver',
+        rating: '2',
+        views: '7',
+        year: '2018',
+        price: '90.7'
+    },
+    {
+        imgUrl: 'https://images.gr-assets.com/books/1474585768l/31146564.jpg',
+        title: 'Vegetables Cookbook',
+        author: 'Matthew Biggs',
+        rating: '5',
+        views: '332',
+        year: '2011',
+        price: '22.3'
+    },
+];
 
+// фильтрация бесплатных книг
+var booksFree = booksData.filter(filterByFree);
 
-    var book = document.createElement('div');
-    book.className = "books";
-    wrapperbooks.appendChild(book);
-
-    var cover = document.createElement('div');
-    cover.className = "cover";
-    cover.style.background = 'url(' + this.cover + ') top left/cover';
-    book.appendChild(cover);
-
-    var title = document.createElement('h3');
-    title.className = "title";
-    title.innerHTML = this.title;
-    book.appendChild(title);
-
-    var author = document.createElement('span');
-    author.className = "author";
-    author.innerHTML = "by " + this.author;
-    book.appendChild(author);
-
-    var rating = document.createElement('div');
-    rating.className = "rating";
-    rating.innerHTML = this.rating;
-    book.appendChild(rating);
-
-    var info = document.createElement('span');
-    info.className = "info";
-    info.innerHTML = "views: " + this.views + " " + "price: " + this.year + " " + "updated: " + this.price;
-    book.appendChild(info);
+function filterByFree(item) {
+    if (item.price === '0') { return true; }
 }
 
+// фильтрация книг по дате
+var booksMostRecent = booksData.slice().sort(compareYear);
 
+function compareYear(a, b) {
+    return b.year - a.year;
+};
 
+var booksMostViews = booksData.slice().sort(compareViews);
 
+// фильтрация просматриваемых книг
+function compareViews(a, b) {
+    return b.views - a.views;
+};
 
-var books = [];
+var booksMostViews = booksData.slice().sort(compareViews);
 
-var book1 = new Book('https://images-na.ssl-images-amazon.com/images/I/51TYJDHEJSL._AC_UL320_SR248,320_.jpg',
-    'Jewels of Nizam', 'Geeta Devi', 3, 20, 2011, 19.5);
-books.push(book1);
-var book2 = new Book('https://b.zmtcdn.com/data/pictures/6/4000236/65f5b0154f1824897086b882a7060f8a_featured_v2.jpg',
-    'Cakes & Bakes', 'Sanjeev Kapoor', 1, 4303, 2013, 22.3);
-books.push(book2);
-var book3 = new Book('https://images-na.ssl-images-amazon.com/images/I/51FqIlv4TaL._SX258_BO1,204,203,200_.jpg',
-    'Jamies Kitchen', 'Jamie Oliver', 5, 3054, 2018, 70.1);
-books.push(book3);
-var book4 = new Book('https://www.freebiefindingmom.com/wp-content/uploads/2015/07/Cheap-meals-for-large-families-pin.jpg',
-    'Inexpensive Family Meals', 'Simon Holst', 3, 3000, 2013, 110);
-books.push(book4);
-var book5 = new Book('https://images-na.ssl-images-amazon.com/images/I/51z%2BXZgMFtL._SX384_BO1,204,203,200_.jpg',
-    'Paleo Slow Cooking', 'Chrissy Gower', 2, 302, 2001, 5.3);
-books.push(book5);
-var book6 = new Book('https://www.betterreading.com.au/wp-content/uploads/2014/10/9781921382192-195x298.jpg',
-    'Cook Like an Italian', 'Tobie Puttock', 3, 22, 2000, 0);
-books.push(book6);
-var book7 = new Book('https://images-na.ssl-images-amazon.com/images/I/51BiwDk0f3L._SX367_BO1,204,203,200_.jpg',
-    'Indian Cooking', 'Geeta Devi', 3, 15, 2007, 30.2);
-books.push(book7);
-var book8 = new Book('https://images-na.ssl-images-amazon.com/images/I/910r6WKtPfL.jpg',
-    'Jamie Does', 'Jamie Oliver', 1, 345, 2011, 0);
-books.push(book8);
-var book9 = new Book('https://images-na.ssl-images-amazon.com/images/I/61AkWIb6ArL._SX258_BO1,204,203,200_.jpg',
-    'Jamies italy', 'Jamie Oliver', 2, 7, 2018, 90.7);
-books.push(book9);
-var book10 = new Book('https://images.gr-assets.com/books/1474585768l/31146564.jpg',
-    'Vegetables Cookbook', 'Matthew Biggs', 5, 332, 2011, 22.3);
-books.push(book10);
+//создание элемента
+function makeElement(tagName, className, text) {
+    var element = document.createElement(tagName);
+    element.classList.add(className);
+    if (text) {
+        element.textContent = text;
+    }
+    return element;
+};
 
-var domBooks = document.querySelectorAll('.books');
+//создание книжной карточки
+function createBook(books) {
+    var listItem = makeElement('li', 'book');
+
+    var picture = makeElement('img', 'book__image');
+    picture.src = books.imgUrl;
+    picture.alt = books.title;
+    listItem.appendChild(picture);
+
+    var title = makeElement('h2', 'book__title', books.title);
+    listItem.appendChild(title);
+
+    var author = makeElement('p', 'book__author');
+    author.textContent = 'by ' + books.author;
+    listItem.appendChild(author);
+
+    var rating = makeElement('p', 'book__rating', books.rating);
+    listItem.appendChild(rating);
+
+    var price = makeElement('p', 'book__info');
+    price.textContent = 'views: ' + books.views + ' price: ' + books.price + ' updated: ' + books.year;
+    listItem.appendChild(price);
+
+    return listItem;
+};
+
+var bookList = document.querySelector('.wrapperbooks__books');
+var bo = document.querySelectorAll('.book');
 
 var filter_select_el = document.getElementsByName('filter');
-for (i = 0; i < 4; i++) {
-    filter_select_el[i].addEventListener('click', setMenuClass);
-}
-//var items_el = document.getElementById('wrapperbooks');
+filter_select_el.forEach(function (item) {
+    item.addEventListener('change', setMenuClass);
+});
 
 function setMenuClass() {
 
     for (var i = 0; i < filter_select_el.length; i++) {
         if (filter_select_el[i].type === "radio" && filter_select_el[i].checked) {
+            
             if (filter_select_el[i].id === 'all') {
-                alert("selected: 1");
+                booksData.forEach(function (item) {
+                    var bookItem = createBook(item);
+                    bookList.appendChild(bookItem);
+                })
             }
             else if (filter_select_el[i].id === 'recent') {
-                alert("selected: 2");
+                booksMostRecent.forEach(function (item) {
+                    var bookItem = createBook(item);
+                    bookList.appendChild(bookItem);
+                })
             }
             else if (filter_select_el[i].id === 'pop') {
-                alert("selected: 3");
+                booksMostViews.forEach(function (item) {
+                    var bookItem = createBook(item);
+                    bookList.appendChild(bookItem);
+                })
             }
             else if (filter_select_el[i].id === 'free') {
-                clear();
+                booksFree.forEach(function (item) {
+                    var bookItem = createBook(item);
+                    bookList.appendChild(bookItem);
+                })
             }
         }
     }
 }
-function clear() {
-    for (i = 0; i < domBooks.length; i++) {
-        wrapperbooks.removeChild(domBooks[i]);
-    }
-}
-    
